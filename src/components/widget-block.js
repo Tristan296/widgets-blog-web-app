590393b8f8ae4e9623e592701ad76a3a1044b2fc
Tristan N <109927879+Tristan296@users.noreply.github.com>
2023-04-05 17:09:27 +1000

    created weather widget

--- src/components/widget-block.js
+++ src/components/widget-block.js
@@ -2,28 +2,95 @@ import { LitElement, html, css } from 'https://cdn.jsdelivr.net/gh/lit/dist@2/co
 
 class WidgetBlock extends LitElement {
   static properties = {
-    header: { type: String },
+      city: { type: String },
+      forecast: { type: Array },
   }
-
-  static styles = css`
-    :host {
-        display: block;
-        width: 250px;
-        height: 250px;
-        background-color: azure;
-    }
-  `;
 
   constructor() {
     super();
-    this.header = 'Widget';
+    this.city = '';
+    this.forecast = [];
+  }
+
+  static styles = css` 
+   .weather-widget {
+     border: 1px solid black;
+     padding: 10px;
+     margin: 10px;
+     width: 500px;
+     display: flex;
+     flex-direction: column;
+   }
+   
+   .weather-widget h2 {
+     margin-top: 0;
+   }
+   
+   .weather-widget input[type=text] {
+     width: 100%;
+     padding: 12px 20px;
+     margin: 8px 0;
+     box-sizing: border-box;
+     border: 2px solid #ccc;
+     border-radius: 4px;
+   }
+   
+   .weather-widget button {
+     background-color: #4CAF50;
+     color: white;
+     padding: 12px 20px;
+     border: none;
+     border-radius: 4px;
+     cursor: pointer;
+   }
+   
+   .weather-widget button:hover {
+     background-color: #45a049;
+   }
+   
+   .weather-widget ul {
+     list-style: none;
+     margin: 0;
+     padding: 0;
+   }
+   
+   .weather-widget li {
+     display: flex;
+     justify-content: space-between;
+     align-items: center;
+     margin-bottom: 10px;
+   }
+  `;
+  
+
+  _fetchForecast() {
+    const url = `https://api.open-meteo.com/v1/forecast?city=${this.city}`;
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        this.forecast = data;
+      });
   }
 
   render() {
     return html`
-        <h3>${this.header}</h3>
+      <div class="widget-block">
+        <h2>Weather Widget</h2>
+        <input type="text" placeholder="Enter city name" @input="${e => this.city = e.target.value}">
+        <button @click="${this._fetchForecast}">Get forecast</button>
+        <ul>
+          ${this.forecast.map(day => html`
+            <li>
+              <span>${day.datetime}</span>
+              <span>${day.temp}°C</span>
+              <span>${day.weather.description}</span>
+            </li>
+          `)}
+        </ul>
+      </div>
     `;
   }
+
 }
 
 customElements.define('widget-block', WidgetBlock);
