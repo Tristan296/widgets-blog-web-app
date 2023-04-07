b006a79bf4adac27ae0e995c82b9d26a4f72ebdd
Tristan N <109927879+Tristan296@users.noreply.github.com>
2023-04-07 12:48:24 +1000

    Created meme widget

--- src/components/widget-block.js
+++ src/components/widget-block.js
@@ -1,96 +1,57 @@
 import { LitElement, html, css } from 'https://cdn.jsdelivr.net/gh/lit/dist@2/core/lit-core.min.js';
 
 class WidgetBlock extends LitElement {
+  static styles = 
+    css`
+      .widget-border {
+        width: 200px;
+        border: 1px solid #ccc;
+        border-radius: 8px;
+        padding: 16px;
+        box-sizing: border-box;
+        text-align: center;
+      }
+
+      .meme-img {
+        width: 100%;
+        height: 160px;
+        object-fit: cover;
+        border-radius: 8px;
+      }
+    `;
+
   static properties = {
-      city: { type: String },
-      forecast: { type: Array },
+      meme: { type: Object }
   }
 
   constructor() {
     super();
-    this.city = '';
-    this.forecast = [];
+    this.meme = null;
   }
 
-  static styles = css` 
-   .weather-widget {
-     border: 1px solid black;
-     padding: 10px;
-     margin: 10px;
-     width: 500px;
-     display: flex;
-     flex-direction: column;
-   }
-   
-   .weather-widget h2 {
-     margin-top: 0;
-   }
-   
-   .weather-widget input[type=text] {
-     width: 100%;
-     padding: 12px 20px;
-     margin: 8px 0;
-     box-sizing: border-box;
-     border: 2px solid #ccc;
-     border-radius: 4px;
-   }
-   
-   .weather-widget button {
-     background-color: #4CAF50;
-     color: white;
-     padding: 12px 20px;
-     border: none;
-     border-radius: 4px;
-     cursor: pointer;
-   }
-   
-   .weather-widget button:hover {
-     background-color: #45a049;
-   }
-   
-   .weather-widget ul {
-     list-style: none;
-     margin: 0;
-     padding: 0;
-   }
-   
-   .weather-widget li {
-     display: flex;
-     justify-content: space-between;
-     align-items: center;
-     margin-bottom: 10px;
-   }
-  `;
-  
+  connectedCallback() {
+    super.connectedCallback();
+    this.fetchMeme();
+  }
 
-  _fetchForecast() {
-    const url = `https://api.open-meteo.com/v1/forecast?city=${this.city}`;
-    fetch(url)
+  fetchMeme() {
+    fetch('https://api.imgflip.com/get_memes')
       .then(response => response.json())
       .then(data => {
-        this.forecast = data;
+        const memes = data.data.memes;
+        const randomIndex = Math.floor(Math.random() * memes.length);
+        this.meme = memes[randomIndex];
       });
   }
 
   render() {
     return html`
-      <div class="widget-block">
-        <h2>Weather Widget</h2>
-        <input type="text" placeholder="Enter city name" @input="${e => this.city = e.target.value}">
-        <button @click="${this._fetchForecast}">Get forecast</button>
-        <ul>
-          ${this.forecast.map(day => html`
-            <li>
-              <span>${day.datetime}</span>
-              <span>${day.temp}°C</span>
-              <span>${day.weather.description}</span>
-            </li>
-          `)}
-        </ul>
+      <div class="widget-border">
+        <img class="meme-img" src="${this.meme.url}" alt="${this.meme.name}">
+        <p>${this.meme.name}</p>
       </div>
     `;
   }
-
 }
 
 customElements.define('widget-block', WidgetBlock);
