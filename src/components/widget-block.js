b076c0ed55a7d156882af2db0ae7f96d3089d236
Tristan N <109927879+Tristan296@users.noreply.github.com>
2023-04-07 15:02:56 +1000

    - Added button to fetch new memes. - import meme-widget.js in portal.js file - created new file 'meme-widget.js' - added reference <meme-widget></meme-widget> in widget-block.js file

--- src/components/widget-block.js
+++ src/components/widget-block.js
@@ -1,55 +1,22 @@
 import { LitElement, html, css } from 'https://cdn.jsdelivr.net/gh/lit/dist@2/core/lit-core.min.js';
 
 class WidgetBlock extends LitElement {
-  static styles = 
-    css`
-      .widget-border {
-        width: 200px;
-        border: 1px solid #ccc;
-        border-radius: 8px;
-        padding: 16px;
-        box-sizing: border-box;
-        text-align: center;
-      }
-
-      .meme-img {
-        width: 100%;
-        height: 160px;
-        object-fit: cover;
-        border-radius: 8px;
-      }
-    `;
-
   static properties = {
-      meme: { type: Object }
+    header: { type: String },
   }
+
+  static styles = css`
+  
+  `;
 
   constructor() {
     super();
-    this.meme = null;
-  }
-
-  connectedCallback() {
-    super.connectedCallback();
-    this.fetchMeme();
-  }
-
-  fetchMeme() {
-    fetch('https://api.imgflip.com/get_memes')
-      .then(response => response.json())
-      .then(data => {
-        const memes = data.data.memes;
-        const randomIndex = Math.floor(Math.random() * memes.length);
-        this.meme = memes[randomIndex];
-      });
+    this.header = 'Widget';
   }
 
   render() {
     return html`
-      <div class="widget-border">
-        <img class="meme-img" src="${this.meme.url}" alt="${this.meme.name}">
-        <p>${this.meme.name}</p>
-      </div>
+        <meme-widget></meme-widget>
     `;
   }
 }
