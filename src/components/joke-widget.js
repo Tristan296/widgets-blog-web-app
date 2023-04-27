713956348c11556de1fed2d163df4444a9b0e64e
brigidmcd <126314897+brigidmcd@users.noreply.github.com>
2023-04-27 10:49:25 +1000

    joke widget edits

--- src/components/joke-widget.js
+++ src/components/joke-widget.js
@@ -1,6 +1,7 @@
 import { LitElement, html, css } from 'https://cdn.jsdelivr.net/gh/lit/dist@2/core/lit-core.min.js';
 
 class JokeWidget extends LitElement {
+
     static styles = 
       css`
         .widget-border {
@@ -15,4 +16,47 @@ class JokeWidget extends LitElement {
       `;
 
       
+  static properties = {
+    setup: {},
+    punchline: {}
+
+}
+
+
+
+/* reveal button just changes to css 
+
+first goal: get both to display  (setup then punchline)
+
+*/
+
+constructor() {
+  super();
+  this._data = null;
+}
+
+connectedCallback() {
+  super.connectedCallback();
+  fetch(`https://official-joke-api.appspot.com/random_joke`)
+  .then(response => response.json())
+  .then(data => {
+    this._data = data;
+  });
+}
+
+render() {
+  
+  return html`
+  <div class="widget-border">
+  <p> JOKE SETUP: ${this.setup}</p>
+  <p> JOKE SETUP: ${this.punchline}</p>
+  </div>`;
+
+}
+}
+
+
+
+      
     }
+    customElements.define('joke-widget', JokeWidget);
