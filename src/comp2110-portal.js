ba01976305f5905eb2596ff6c0c17bcbfdb3fa49
Tristan N <109927879+Tristan296@users.noreply.github.com>
2023-05-04 09:12:55 +1000

    Added nathan's trivia-widget

--- src/comp2110-portal.js
+++ src/comp2110-portal.js
@@ -5,6 +5,9 @@ import './components/widget-column.js';
 import './components/ad-widget.js';
 import './components/login-widget.js';
 
+//trivia widget
+import './components/trivia-widget.js';
+
 //trufact widget
 import './components/trufact-widget.js';
 
