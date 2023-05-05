ca85275038013622ffbfacb9bbb1411c3dfd475b
staceypurcell <88125374+stacey-p@users.noreply.github.com>
2023-05-05 15:33:57 +1000

    created basic blog post form using correct auth

--- src/components/new-post.js
+++ src/components/new-post.js
@@ -3,21 +3,17 @@ import { LitElement, html, css } from 'https://cdn.jsdelivr.net/gh/lit/dist@2/co
 
 class NewPost extends LitElement {
     static properties = {
-        _data: { state: true },
         _user: { type: String, state: true }, 
-        _title: { type: String, state: true },
     }
 
     constructor() {
         super();
         this._user = getUser();
-        this._data = null;
-        this._title = null;
     }
 
-    postBlog() {
-        const blogPost = this._data.name;
-        const title = this._title;
+    postBlog(event) {
+        const blogPost = event.target.blogpost.value;
+        const title = event.target.title.value;
         const endpoint = "https://comp2110-portal-server.fly.dev/blog";
         const Authorization = "Basic " + getUser().token;
 
@@ -38,8 +34,7 @@ class NewPost extends LitElement {
         })
             .then(response => response.json())
             .then(data => {
-                console.log('blog posted:', data);
-                location.reload();})
+                console.log('blog posted:', data);})
             .catch(error => {
                 console.error('Error posting to blog:', error);
             });
@@ -48,7 +43,11 @@ class NewPost extends LitElement {
     render(){
         return html`<p>This is currently a placeholder and will be relocated / moved.
         </p>
-        <form>blogpost<input type=text id=title><input type=textarea id=blogpost></form>`;
+        <form @submit=${this.postBlog}>blogpost
+        <input name="title" type=text id=title>
+        <input name="blog" type=textarea id=blogpost>
+        <input name="button" type='submit' value='post to blog'>
+        </form>`;
     }
 }
 
