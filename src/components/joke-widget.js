db278f244a23c66b3737812ba2529f335648e4ee
brigidmcd <126314897+brigidmcd@users.noreply.github.com>
2023-05-13 16:02:41 +1000

    formatting changes

--- src/components/joke-widget.js
+++ src/components/joke-widget.js
@@ -79,7 +79,7 @@ VV review below style VV*/
   }
 
 
-  
+
   fetchJoke() {
     fetch(`https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,racist,sexist,explicit&type=twopart`)
       .then(response => response.json())
@@ -97,9 +97,9 @@ VV review below style VV*/
   <h2>Joke!</h2>
   <p> <b> ${this._data.setup} </b> </p>
   <div ?hidden="${!this.visible}">
+
   <p> ${this._data.delivery} </p>
 </div>
-
 <button @click="${this.togglePunchline}">${this.visible ? 'Hide Punchline' : 'Show Punchline'}</button>
 
 <button @click="${this.getNewJoke}">Show New Joke</button>
@@ -124,7 +124,7 @@ VV review below style VV*/
   //function to hide and show punchline
   togglePunchline() {
     this.visible = !this.visible;
-    const text = this.shadowRoot.querySelector('div ?hidden');
+    const text = this.shadowRoot.querySelector('div hidden');
     text.hidden = !this.visible;
   }
 }
